9e5212104a391de936c6a2916bf1fc3c955a0ef4
yaroslavChmil <63856133+yaroslavChmil@users.noreply.github.com>
2020-09-16 17:35:55 +0300

    Home Task

---
+++ src/Lesson3/TicTacToe.java
@@ -0,0 +1,139 @@
+package Lesson3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class TicTacToe {
+    private static final char DOT_HUMAN = 'X';
+    private static final char DOT_AI = 'O';
+    private static final char DOT_EMPTY = '.';
+    private static final char DOTS_TO_WIN = 3;
+    private static final int SIZE = 3;
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final Random RANDOM = new Random();
+    private static char[][] field;
+    private static int fieldSizeX;
+    private static int fieldSizeY;
+
+    //init field
+    private static void initField() {
+        fieldSizeY = 3;
+        fieldSizeX = 3;
+        field = new char[fieldSizeY][fieldSizeX];
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                field[y][x] = DOT_EMPTY;
+            }
+        }
+    }
+
+    // printField
+    private static void printField() {
+        System.out.print("+");
+        for (int i = 0; i < fieldSizeX * 2 + 1; i++)
+            System.out.print((i % 2 == 0) ? "-" : i / 2 + 1);
+        System.out.println();
+
+        for (int i = 0; i < fieldSizeY; i++) {
+            System.out.print(i + 1 + "|");
+            for (int j = 0; j < fieldSizeX; j++)
+                System.out.print(field[i][j] + "|");
+            System.out.println();
+        }
+
+        for (int i = 0; i <= fieldSizeX * 2 + 1; i++)
+            System.out.print("-");
+        System.out.println();
+    }
+
+    // humanTurn
+    private static void humanTurn() {
+        int x;
+        int y;
+        do {
+            System.out.print("Введите координаты хода X и Y (от 1 до 3) через пробел: ");
+            x = SCANNER.nextInt() - 1;
+            y = SCANNER.nextInt() - 1;
+        } while (!isCellValid(x, y) || !isCellEmpty(x, y));
+        field[y][x] = DOT_HUMAN;
+    }
+
+    private static boolean isCellEmpty(int x, int y) {
+        return field[y][x] == DOT_EMPTY;
+    }
+
+    private static boolean isCellValid(int x, int y) {
+        return x >= 0 && x < fieldSizeX && y >= 0 && y < fieldSizeY;
+    }
+
+    // aiTurn
+    private static void aiTurn() {
+        int x;
+        int y;
+        do {
+            x = RANDOM.nextInt(fieldSizeX);
+            y = RANDOM.nextInt(fieldSizeY);
+        } while (!isCellEmpty(x, y));
+        field[y][x] = DOT_AI;
+    }
+
+    // checkWin
+    private static boolean checkWin(char dot) {
+        int countDotsDiagonal = 0;
+        int countDotsHorizontal;
+        int countDotsVertical;
+        for (int i = 0; i < SIZE; i++) {
+            if(field[i][i] == dot) countDotsDiagonal++; //проверяем диагонали
+            else if(field[i][SIZE - i - 1] == dot) countDotsDiagonal++;
+            countDotsHorizontal = 0;
+            countDotsVertical = 0;
+            for (int j = 0; j < SIZE; j++) {
+                if (field[i][j] == dot) countDotsHorizontal++; //проверяем вертикаль и горизонталь
+                if (field[j][i] == dot) countDotsVertical++;
+            }
+            if(countDotsHorizontal == DOTS_TO_WIN) return true;
+            if(countDotsVertical == DOTS_TO_WIN) return true;
+        }
+        return countDotsDiagonal == DOTS_TO_WIN;
+    }
+
+    //checkDraw
+    private static boolean checkDraw() {
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                if (isCellEmpty(x, y)) return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        while (true) {
+            initField();
+            printField();
+            while (true) {
+                humanTurn();
+                printField();
+                if (gameChecks(DOT_HUMAN, "Human win")) break;
+                aiTurn();
+                printField();
+                if (gameChecks(DOT_AI, "Computer win")) break;
+            }
+            System.out.println("Play again?");
+            if (!SCANNER.next().equals("Y"))
+                break;
+        }
+    }
+
+    private static boolean gameChecks(char dot, String s) {
+        if (checkWin(dot)) {
+            System.out.println(s);
+            return true;
+        }
+        if (checkDraw()) {
+            System.out.println("draw!");
+            return true;
+        }
+        return false;
+    }
+}
